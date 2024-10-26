8f755e408dd53d368993294a9e9f5844c816c281
TakayukiH4R4 <sthaikgaoytuok4i@gmail.com>
2024-10-26 22:14:56 +0900

    2024/10/26

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,87 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		//Hello world の出力
+		System.out.println("Hello world!!");
+
+		System.out.println("おはよう。");
+		System.out.println(0);
+		System.out.println("例外が発生しました。");
+		System.out.println("例外が");
+		System.out.println("発生しました");
+		System.out.println("例外が\r\n発生しました");
+
+
+		//第４講
+
+		int suugaku = 10;
+		int eigo = 82;
+		if(suugaku > 50 ||eigo >50){
+			System.out.println("この人は合格です");
+		}
+
+		String name = "高橋";
+		System.out.println(name);
+
+
+		//いろんなプリミティブ値
+		String str2 ="Tanaka";
+		int num1 = 12345;
+		char char1 ='A';
+		boolean boo = true;
+
+		System.out.println(str2);
+		System.out.println(num1);
+		System.out.println(char1);
+		System.out.println(boo);
+
+
+		//第５講
+
+		int a4 = 10/3;
+		System.out.println(a4);
+
+		float a5 =(float)10/3;
+		System.out.println(a5);
+
+		double a6 =(double)10/3;
+		System.out.println(a6);
+
+		int a7 =7%3;
+		System.out.println(a7);
+
+		//第６講
+
+
+		int a8 = 10;
+		int a9 = 20;
+		System.out.println(++a8);
+		System.out.println(++a8);
+		System.out.println(++a8);
+		System.out.println(++a8);
+
+		System.out.println(a8++);
+		System.out.println(a8++);
+		System.out.println(a8++);
+		System.out.println(a8++);
+
+		System.out.println(--a9);
+		System.out.println(--a9);
+		System.out.println(--a9);
+		System.out.println(--a9);
+
+
+		System.out.println(a9--);
+		System.out.println(a9--);
+		System.out.println(a9--);
+		System.out.println(a9--);
+	}
+
+
+
+
+}
