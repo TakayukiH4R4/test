8f216eefc320c1f9339cecbf92a06e4e31ed5ab2
TakayukiH4R4 <sthaikgaoytuok4i@gmail.com>
2024-11-08 21:13:08 +0900

    2024_11_08

--- TestProject01/src/practice3.java
+++ TestProject01/src/practice3.java
@@ -1,9 +1,30 @@
 
 public class practice3 {
 
-	public static int main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
+		public static int totalPrice(int fruitprice, double tax, int haiso) {
+			 return (int)(fruitprice * tax) + haiso ;
+			 }
+
+		public static String hello(){
+			return "こんにちは";
+		}
+
+
+		public static void main(String[] args) {
+			// TODO 自動生成されたメソッド・スタブ
+
+
+			System.out.println(totalPrice(300,1.08,350));
+			System.out.println(totalPrice(450,1.08,350));
+			System.out.println(totalPrice(400,1.08,350));
+			System.out.println(totalPrice(350,1.08,350));
+
+			System.out.println("山田さん。"+hello());
+
+
+//ここまでが記述範囲
+		}
 	}
 
-}
+
