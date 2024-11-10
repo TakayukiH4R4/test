77c599962820184acf76eb69b92235248dadb4a5
TakayukiH4R4 <sthaikgaoytuok4i@gmail.com>
2024-11-10 22:04:29 +0900

    2024_11_10

--- TestProject01/src/jp/co/DIworks/action/main.java
+++ TestProject01/src/jp/co/DIworks/action/main.java
@@ -5,6 +5,35 @@ public class main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		System.out.println("Hello world");
+		System.out.println(gokei());
+
+		int total = gokei();
+		System.out.println(total);
+
+		System.out.println(gokei_2(3, 4));
+
+		System.out.println(en_menseki(4));
+
+
+	//ここで記述は終了
 	}
 
-}
+	//ここにメソッドの記述
+
+	public static int gokei() {
+		return 1+1;
+	}
+
+	public static int gokei_2(int number1, int number2) {
+		return number1 + number2;
+	}
+
+	public static double en_menseki(int hankei) {
+		return hankei * hankei * 3.14;
+	}
+
+		//ここでメソッドの記述は終了
+	}
+
+
